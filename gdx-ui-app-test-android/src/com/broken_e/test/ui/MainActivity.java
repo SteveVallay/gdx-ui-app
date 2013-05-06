8b007d3bdfe94ef5635637b81eaea047b8872816
treymiller <treyy333@gmail.com>
2013-05-06 12:44:15 -0700

    got android project working (neglected it before.. oops)

--- gdx-ui-app-test-android/src/com/broken_e/test/ui/MainActivity.java
+++ gdx-ui-app-test-android/src/com/broken_e/test/ui/MainActivity.java
@@ -1,12 +1,13 @@
-package com.broken_e.ui;
+package com.broken_e.test.ui;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import com.broken_e.test.ui.TestApp;
 
-public class AndroidStarter extends AndroidApplication {
+public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
